4cb74829c6df74002fb028618c28d03eedde06e5
serafinpl <ser.burberibar@gmail.com>
2021-05-15 00:56:25 +0200

    theme clean

--- src/theme.js
+++ src/theme.js
@@ -50,41 +50,15 @@ export default {
       lineHeight: 'body',
       fontWeight: 'body'
     },
-    p: {
-      color: 'text',
-      fontFamily: 'body',
-      fontWeight: 'body',
-      lineHeight: 'body'
-    },
+    
     a: {
       color: 'primary'
     },
-    pre: {
-      fontFamily: 'monospace',
-      overflowX: 'auto',
-      code: {
-        color: 'inherit'
-      }
-    },
+    
     code: {
       fontFamily: 'monospace',
       fontSize: 'inherit'
     },
-    table: {
-      width: '100%',
-      borderCollapse: 'separate',
-      borderSpacing: 0
-    },
-    th: {
-      textAlign: 'left',
-      borderBottomStyle: 'solid'
-    },
-    td: {
-      textAlign: 'left',
-      borderBottomStyle: 'solid'
-    },
-    img: {
-      maxWidth: '100%'
-    }
+    
   }
 }
