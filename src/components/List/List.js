034f41222e853422288eca3617ebfdf5d461e5ad
serafinpl <ser.burberibar@gmail.com>
2021-05-15 23:49:40 +0200

    adding files

---
+++ src/components/List/List.js
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react'
+import {atom, selector, useRecoilState} from 'recoil';
+
+import { Container, Checkbox, Text, Label,Button  } from 'theme-ui'
+import Task from '../Task/Task';
+ 
+import {NavLink} from 'react-router-dom';
+
+
+const List = ({list}) => {
+  let view= null;
+  if (list) {
+          view = list.map((todoItem) => (
+            <Task key={todoItem.id} task={todoItem} ></Task>
+          ));
+
+        }
+
+  return(
+    	<Container  bg="muted">
+        
+        {view}
+      </Container>
+  );
+
+}
+
+export default List;
