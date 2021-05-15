1eadf5cdd95de42a8ff7f231930a200d5de17d3e
serafinpl <ser.burberibar@gmail.com>
2021-05-16 00:18:06 +0200

    fulltask view add

---
+++ src/components/FullTask/FullTask.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react'
+import {atom, selector, useRecoilState} from 'recoil';
+
+import { Container, Checkbox, Text, Label,Button  } from 'theme-ui'
+
+ 
+import {NavLink} from 'react-router-dom';
+
+
+const FullTask = ({task}) => {
+
+  return(
+    	<Container  bg="muted">
+        <Label>
+          <Checkbox defaultChecked={task.completed} />
+
+          <NavLink to={'/'+task.id}>
+            <Text sx={{
+              flexGrow: 1,
+            }}>{task.title}</Text>
+          </NavLink>
+          
+        </Label>
+        
+      </Container>
+  );
+
+}
+
+export default FullTask;
