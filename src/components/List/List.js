f4bd45e93df7815853545c7e5b80a60c1459d7a3
serafinpl <ser.burberibar@gmail.com>
2021-05-20 16:56:16 +0200

    add select

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,7 @@
-import React, {useEffect,Suspense} from 'react'
-import {atom, selector, useRecoilState} from 'recoil';
+import React, {useEffect,Suspense,useState} from 'react'
+import {atom, selector, useRecoilState, useRecoilValue} from 'recoil';
 
-import { Flex,Box, Container, Checkbox, Text, Label,Button, Spinner, Input  } from 'theme-ui'
+import { Flex,Box, Container, Checkbox, Text, Label,Button, Spinner, Input,Paragraph  } from 'theme-ui'
 import Task from '../Task/Task';
  
 import axios from '../../axios';
@@ -12,17 +12,61 @@ import {Route, Switch, Redirect} from 'react-router-dom';
 
 import {listStateMain} from '../../recoliState';
 
+const todoListSearch = atom({
+  key: 'todoListSearch',
+  default: '',
+});
+
+const searchState = selector({
+  key: 'searchState', 
+  get: ({get}) => {
+    const list = get(listStateMain);
+    const searchQuery = get(todoListSearch);
+    
+    return (list.filter(item => item.title.search(searchQuery) !== -1));
+  },
+});
+
+const ListStats = selector({
+  key: 'ListStats',
+  get: ({get}) => {
+    const list = get(listStateMain);
+    const totalNum = list.length;
+    const totalCompletedNum = list.filter((item) => item.completed).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    console.log(totalNum,
+      totalCompletedNum,
+      totalUncompletedNum)
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+    };
+  },
+});
+
+
+
 
 
 
 const List = (props) => {
-  
+
+  const [searchInput, setSearchInput] = useRecoilState(todoListSearch);
+  const [selectView, setSelectView] = useRecoilState(todoListSearch);
+
   const [todoList, setTodoList] = useRecoilState(listStateMain);
+
+  const searchResult = useRecoilValue(searchState);
+
+  const stats = useRecoilValue(ListStats);
+
+  console.log(searchResult);
 
   let view = <Spinner sx={{margin: '0 auto',}}/>;
 
   if (todoList.length > 0) {
-    view = todoList.map((todoItem) => (
+    view = searchResult.map((todoItem) => (
       <Task  key={todoItem.id} task={todoItem} ></Task>
     ));
 
@@ -30,8 +74,14 @@ const List = (props) => {
 
   return(
   	<Flex sx={{ flexDirection: 'column', height: '100vh', position: 'fixed', width: '100vw',}} bg="muted">
-      <Box sx={{margin: '5px'}}>
-        <Input placeholder='Wyszukaj'/>
+      <Box sx={{padding: '5px', textAlign: 'right', borderBottom: '1px solid #442929', bg: '#cfe3cd'}}>
+        <Input placeholder='Wyszukaj' value={searchInput} onChange={event => setSearchInput(event.target.value)}/>
+        <Select value={selectView} onChange={(event) => {setSelectView(event.target.value)}}>
+          <option>Wszystkie</option>
+          <option>Wykonane</option>
+          <option>Niewykonane</option>
+        </Select>
+        <Paragraph>{`Wykonane: ${stats.totalCompletedNum} / Niewykonane ${stats.totalUncompletedNum}`}</Paragraph>
       </Box>
       <Box sx={{ flexGrow: 1, overflowY: 'auto', textAlign: 'center'}}>
         {view}
