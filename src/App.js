afa9d672ac3f0ee96ad4151d517d0b59d0a652b8
serafinpl <ser.burberibar@gmail.com>
2021-05-20 23:04:56 +0200

    cleaning app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-
-import React, {useState, useEffect, Suspense} from 'react'
 import './App.css';
-import axios from './axios';
 
 import Main from './components/Main/Main';
-import Navi from './components/Navi/Navi';
 
-import {RecoilRoot, atom, selector, useRecoilState} from 'recoil';
+import {RecoilRoot} from 'recoil';
 
-import {Route, HashRouter, Switch, Redirect} from 'react-router-dom';
-
-import { Spinner } from 'theme-ui';
 
 import { ThemeProvider } from 'theme-ui'
 import theme from './theme'
