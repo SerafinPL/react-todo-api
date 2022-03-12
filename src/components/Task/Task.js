aa57cb28b5dc817066965e96ff9b71ad5f63d4dd
serafinpl <ser.burberibar@gmail.com>
2022-03-12 23:05:45 +0100

    env is nice

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,67 +1,65 @@
-import React from 'react'
-import {useRecoilState} from 'recoil';
+import React from "react";
+import { useRecoilState } from "recoil";
 
-import axios from '../../axios';
+import axios from "../../axios";
 
- import {  Checkbox, Text, Label, Message  } from 'theme-ui';
+import { Checkbox, Text, Label, Message } from "theme-ui";
 
-import {NavLink} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import {listStateMain} from '../../recoliState';
+import { listStateMain } from "../../recoliState";
 
-const Task = ({task}) => {
-
-  const firstTime = false
-
-
-
-
-  
-
+const Task = ({ task }) => {
   const [todoList, setTodoList] = useRecoilState(listStateMain);
   const index = todoList.findIndex((listItem) => listItem === task);
-  
-  const clickCheckbox = (checked) => {
 
+  const clickCheckbox = (checked) => {
     const newList = replaceItemAtIndex(todoList, index, {
       ...task,
-      status: checked ? 'completed' : 'pending',
-      
+      status: checked ? "completed" : "pending",
+      completed: checked,
     });
 
     setTodoList(newList);
 
-       
-    axios.put('/todos/'+task.id , {status: checked ? 'completed' : 'pending', title:task.title, user_id:"1000", user: 'KubaKoder', })
-    .then(res => {
-      console.log(res);
-          
-    })
-    .catch(err => console.log(err));
-  }
+    axios
+      .put("/todos/" + task.id, {
+        status: checked ? "completed" : "pending",
+        title: task.title,
+        user_id: "1000",
+        user: "KubaKoder",
+      })
+      .then((res) => {
+        // console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
 
   const replaceItemAtIndex = (arr, index, newValue) => {
     return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
-  }
+  };
 
-  console.log(task.status)
+  return (
+    <Message sx={{ margin: 15 }} bg="mess">
+      <Label>
+        <Checkbox
+          checked={task.status === "completed" ? true : false}
+          onChange={(event) => clickCheckbox(event.target.checked)}
+        />
 
-  return(
-    	<Message  sx={{margin: 15}} bg="mess">
-        <Label>
-          <Checkbox checked={task.status === 'completed' ? true : false} onChange={event => clickCheckbox(event.target.checked)}/>
-
-          <NavLink style={{textDecoration: 'none'}} to={'/'+task.id} >
-            <Text sx={{
-              flexGrow: 1, color: 'tasks'
-            }}>{task.title}</Text>
-          </NavLink>
-          
-        </Label>
-        
-      </Message>
+        <NavLink style={{ textDecoration: "none" }} to={"/" + task.id}>
+          <Text
+            sx={{
+              flexGrow: 1,
+              color: "tasks",
+            }}
+          >
+            {task.title}
+          </Text>
+        </NavLink>
+      </Label>
+    </Message>
   );
-
-}
+};
 
 export default Task;
